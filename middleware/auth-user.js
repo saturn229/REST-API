668b85e2d2b431acc090eace2314aaf9e4a782e5
Matthew Akroyd <matthewakroyd@matthews-mbp.lan>
2024-05-15 14:16:59 -0400

    third commit

---
+++ middleware/auth-user.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const auth = require('basic-auth');
+const bcrypt = require('bcrypt');
+const { User } = require('../models');
+
+// Middleware to authenticate the request using Basic Authentication.
+exports.authenticateUser = async(req, res, next) => {
+    //TODO
+    let message; // store the message to display
+
+    // Parse the user's credentials from the Authorization header.
+    const credentials = auth(req);
+    // If the user's credentials are available...
+        // Attempt to retrieve the user from the data store
+        // by their username (i.e. the user's "key"
+        // from the Authorization header).
+        if(credentials){
+            const user = await User.findOne({ where: {username: credentials.name}});
+            // If a user was successfully retrieved from the data store...
+            // Use the bcrypt npm package to compare the user's password
+            // (from the Authorization header) to the user's password
+            // that was retrieved from the data store.
+            if( user ){
+                const authenticated = bcrypt.compareSync(credentials.pass, user.confirmedPassword);
+                if(authenticated){
+                    console.log(`Authentication successful for username: ${user.username}`);
+
+                    // If the passwords match...
+                    // Store the retrieved user object on the request object
+                    // so any middleware functions that follow this middleware function
+                    // will have access to the user's information.
+                    req.currentUser = user;
+                } else {
+                    message = `Authentication failure for username: ${user.username}`;
+                }
+
+            } else {
+                message = `User not found for username: ${credentials.name}`;
+            }
+        } else {
+            message = 'Auth header not found';
+        }
+
+        if(message){
+            // If user authentication failed...
+            // Return a response with a 401 Unauthorized HTTP status code.
+            console.warn(message);
+            res.status(401).json({ message: 'Access Denied'});
+        } else {
+            // Or if user authentication succeeded...
+            next();
+        }
+};
---
+++ routes.js
@@ -0,0 +1,48 @@
+'use strict';
+
+
+const express = require('express');
+const { asyncHandler } = require('../middleware/async-handler');
+const { User, Course } = require('../models');
+const { authenticateUser } = require('../middleware/auth-user');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+
+//return a list of users
+router.get('/users', authenticateUser, asyncHandler(async(req, res) => {
+    const user = req.currentUser;
+
+    res.json({
+        firstName: user.firstName,
+        lastName: user.lastName,
+        username: user.email,
+        password: user.password
+    });
+    res.status(200);
+    res.end();
+}));
+
+
+router.post('/users', asyncHandler(async(req, red) => {
+    try {
+        let newUser = {
+            "firstName": req.body.firstName,
+            "lastName": req.body.lastName,
+            "email": req.body.email,
+            "password": req.body.password
+        };
+
+        await User.create(req.body);
+        res.status(201).json({ "message": "Account successfully created!" });
+    } catch (error){
+        if (error.name === 'SequelizeValidationError' || error.name === 'SequelizeUniqueConstraintError') {
+            const errors = error.errors.map(err => err.message);
+            res.status(400).json({ errors });   
+          } else {
+            throw error;
+          }
+    }
+}));
+
+//return a list of courses
